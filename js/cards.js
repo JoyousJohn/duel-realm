69f708adc31a511dee99f563984c25e101f662e0
Happy <john.happy@rutgers.edu>
2026-08-17 04:39:32 -0400

    feat(cards): standardize 2:3 card assets, add Field Spells (Mountain, Sogen), and update prompt rules

--- js/cards.js
+++ js/cards.js
@@ -1,201 +1,486 @@
 var cards = { 
     'blue-eyes': {
+        'id': 'blue-eyes',
         'type': 'monsters',
-        'file': 'blue-eyes.png',
+        'file': 'blue_eyes_white_dragon.png',
         'name': 'Blue-Eyes White Dragon',
+        'monsterType': 'Dragon',
+        'attribute': 'LIGHT',
         'level': 8,
         'atk': 3000,
         'def': 2500
     },
 
     'flame-swordsman': {
+        'id': 'flame-swordsman',
         'type': 'monsters',
-        'file': 'flame swordsman.png',
+        'file': 'flame_swordsman.png',
         'name': 'Flame Swordsman',
+        'monsterType': 'Warrior',
+        'attribute': 'FIRE',
         'level': 5,
         'atk': 1800,
-        'def': 1600,
+        'def': 1600
     },
 
     'hitotsu-me-giant': {
+        'id': 'hitotsu-me-giant',
         'type': 'monsters',
-        'file': 'hitotsu.png',
+        'file': 'hitotsu_me_giant.png',
         'name': 'Hitotsu-Me Giant',
+        'monsterType': 'Beast-Warrior',
+        'attribute': 'EARTH',
         'level': 4,
         'atk': 1200,
-        'def': 1000,
+        'def': 1000
     },
 
     'skull-servant': {
+        'id': 'skull-servant',
         'type': 'monsters',
-        'file': 'skull.png',
+        'file': 'skull_servant.png',
         'name': 'Skull Servant',
+        'monsterType': 'Zombie',
+        'attribute': 'DARK',
         'level': 1,
         'atk': 300,
         'def': 200
     },
 
     'tri-horned-dragon': {
+        'id': 'tri-horned-dragon',
         'type': 'monsters',
-        'file': 'tri.png',
+        'file': 'tri_horned_dragon.png',
         'name': 'Tri-Horned Dragon',
+        'monsterType': 'Dragon',
+        'attribute': 'DARK',
         'level': 8,
         'atk': 2850,
-        'def': 2350,
+        'def': 2350
     },
 
     'dark-magician': {
+        'id': 'dark-magician',
         'type': 'monsters',
-        'file': 'dark magician.png',
+        'file': 'dark_magician.png',
         'name': 'Dark Magician',
+        'monsterType': 'Spellcaster',
+        'attribute': 'DARK',
         'level': 7,
         'atk': 2500,
-        'def': 2100,
+        'def': 2100
     },
 
     'gaia-the-fierce-knight': {
+        'id': 'gaia-the-fierce-knight',
         'type': 'monsters',
-        'file': 'gaia.png',
+        'file': 'gaia_the_fierce_knight.png',
         'name': 'Gaia The Fierce Knight',
+        'monsterType': 'Warrior',
+        'attribute': 'EARTH',
         'level': 7,
         'atk': 2300,
         'def': 2100
     },
 
     'celtic-guardian': {
+        'id': 'celtic-guardian',
         'type': 'monsters',
-        'file': 'celtic.png',
+        'file': 'celtic_guardian.png',
         'name': 'Celtic Guardian',
+        'monsterType': 'Warrior',
+        'attribute': 'EARTH',
         'level': 4,
         'atk': 1400,
         'def': 1200
     },
 
     'basic-insect': {
+        'id': 'basic-insect',
         'type': 'monsters',
-        'file': 'basic insect.png',
+        'file': 'basic_insect.png',
         'name': 'Basic Insect',
+        'monsterType': 'Insect',
+        'attribute': 'EARTH',
         'level': 2,
         'atk': 500,
-        'def': 700,
+        'def': 700
     },
 
     'mammoth-graveyard': {
+        'id': 'mammoth-graveyard',
         'type': 'monsters',
-        'file': 'mammoth graveyard.png',
+        'file': 'mammoth_graveyard.png',
         'name': 'Mammoth Graveyard',
+        'monsterType': 'Dinosaur',
+        'attribute': 'EARTH',
         'level': 3,
         'atk': 1200,
-        'def': 800,
+        'def': 800
     },
 
     'silver-fang': {
+        'id': 'silver-fang',
         'type': 'monsters',
-        'file': 'silver fang.png',
+        'file': 'silver_fang.png',
         'name': 'Silver Fang',
+        'monsterType': 'Beast',
+        'attribute': 'EARTH',
         'level': 3,
         'atk': 1200,
-        'def': 800,
+        'def': 800
     },
 
     'red-eyes': {
+        'id': 'red-eyes',
         'type': 'monsters',
-        'file': 'red eyes.png',
+        'file': 'red_eyes_black_dragon.png',
         'name': 'Red-Eyes Black Dragon',
+        'monsterType': 'Dragon',
+        'attribute': 'DARK',
         'level': 7,
         'atk': 2400,
         'def': 2000
     },
 
     'curse-of-dragon': {
+        'id': 'curse-of-dragon',
         'type': 'monsters',
-        'file': 'curse of dragon.png',
+        'file': 'curse_of_dragon.png',
         'name': 'Curse Of Dragon',
+        'monsterType': 'Dragon',
+        'attribute': 'DARK',
         'level': 5,
         'atk': 2000,
         'def': 1500
     },
 
     'dark-gray': {
+        'id': 'dark-gray',
         'type': 'monsters',
-        'file': 'dark gray.png',
+        'file': 'dark_gray.png',
         'name': 'Dark Gray',
+        'monsterType': 'Beast',
+        'attribute': 'EARTH',
         'level': 3,
         'atk': 800,
         'def': 900
     },
 
     'trial-of-nightmare': {
+        'id': 'trial-of-nightmare',
         'type': 'monsters',
-        'file': 'trial of nightmare.png',
+        'file': 'trial_of_nightmare.png',
         'name': 'Trial of Nightmare',
+        'monsterType': 'Fiend',
+        'attribute': 'DARK',
         'level': 4,
         'atk': 1300,
         'def': 900
     },
 
     'nemuriko': {
+        'id': 'nemuriko',
         'type': 'monsters',
         'file': 'nemuriko.png',
         'name': 'Nemuriko',
+        'monsterType': 'Spellcaster',
+        'attribute': 'DARK',
         'level': 3,
         'atk': 800,
         'def': 700
     },
 
-    'trap-hold': {
-        'type': 'traps',
-        'file': 'trap hole.png',
-        'name': 'Trap Hole',
-        'desc': 'When your oponent Normal or Flip Summons a monster with 1000 or more ATK, destroy that monster.'
+    'summoned-skull': {
+        'id': 'summoned-skull',
+        'type': 'monsters',
+        'file': 'summoned_skull.png',
+        'name': 'Summoned Skull',
+        'monsterType': 'Fiend',
+        'attribute': 'DARK',
+        'level': 6,
+        'atk': 2500,
+        'def': 1200
     },
 
-    'dragon-capture-har': {
+    'beaver-warrior': {
+        'id': 'beaver-warrior',
+        'type': 'monsters',
+        'file': 'beaver_warrior.png',
+        'name': 'Beaver Warrior',
+        'monsterType': 'Beast-Warrior',
+        'attribute': 'EARTH',
+        'level': 4,
+        'atk': 1200,
+        'def': 1500
+    },
+
+    'feral-imp': {
+        'id': 'feral-imp',
+        'type': 'monsters',
+        'file': 'feral_imp.png',
+        'name': 'Feral Imp',
+        'monsterType': 'Fiend',
+        'attribute': 'DARK',
+        'level': 4,
+        'atk': 1300,
+        'def': 1400
+    },
+
+    'baby-dragon': {
+        'id': 'baby-dragon',
+        'type': 'monsters',
+        'file': 'baby_dragon.png',
+        'name': 'Baby Dragon',
+        'monsterType': 'Dragon',
+        'attribute': 'WIND',
+        'level': 3,
+        'atk': 1200,
+        'def': 700
+    },
+
+    'time-wizard': {
+        'id': 'time-wizard',
+        'type': 'monsters',
+        'file': 'time_wizard.png',
+        'name': 'Time Wizard',
+        'monsterType': 'Spellcaster',
+        'attribute': 'LIGHT',
+        'level': 2,
+        'atk': 500,
+        'def': 400
+    },
+
+    'kuriboh': {
+        'id': 'kuriboh',
+        'type': 'monsters',
+        'file': 'kuriboh.png',
+        'name': 'Kuriboh',
+        'monsterType': 'Fiend',
+        'attribute': 'DARK',
+        'level': 1,
+        'atk': 300,
+        'def': 200
+    },
+
+    'winged-dragon': {
+        'id': 'winged-dragon',
+        'type': 'monsters',
+        'file': 'winged_dragon.png',
+        'name': 'Winged Dragon, Guardian of the Fortress #1',
+        'monsterType': 'Dragon',
+        'attribute': 'LIGHT',
+        'level': 4,
+        'atk': 1400,
+        'def': 1200
+    },
+
+    'buster-blader': {
+        'id': 'buster-blader',
+        'type': 'monsters',
+        'file': 'buster_blader.png',
+        'name': 'Buster Blader',
+        'monsterType': 'Warrior',
+        'attribute': 'EARTH',
+        'level': 7,
+        'atk': 2600,
+        'def': 2300
+    },
+
+    'harpie-lady': {
+        'id': 'harpie-lady',
+        'type': 'monsters',
+        'file': 'harpie_lady.png',
+        'name': 'Harpie Lady',
+        'monsterType': 'Winged Beast',
+        'attribute': 'WIND',
+        'level': 4,
+        'atk': 1300,
+        'def': 1400
+    },
+
+    'mystical-elf': {
+        'id': 'mystical-elf',
+        'type': 'monsters',
+        'file': 'mystical_elf.png',
+        'name': 'Mystical Elf',
+        'monsterType': 'Spellcaster',
+        'attribute': 'LIGHT',
+        'level': 4,
+        'atk': 800,
+        'def': 2000
+    },
+
+    'man-eater-bug': {
+        'id': 'man-eater-bug',
+        'type': 'monsters',
+        'file': 'man_eater_bug.jpg',
+        'name': 'Man-Eater Bug',
+        'monsterType': 'Insect',
+        'attribute': 'EARTH',
+        'level': 2,
+        'atk': 450,
+        'def': 600
+    },
+
+    'battle-ox': {
+        'id': 'battle-ox',
+        'type': 'monsters',
+        'file': 'battle_ox.png',
+        'name': 'Battle Ox',
+        'monsterType': 'Beast-Warrior',
+        'attribute': 'EARTH',
+        'level': 4,
+        'atk': 1700,
+        'def': 1000
+    },
+
+    'jinzo': {
+        'id': 'jinzo',
+        'type': 'monsters',
+        'file': 'jinzo.png',
+        'name': 'Jinzo',
+        'monsterType': 'Machine',
+        'attribute': 'DARK',
+        'level': 6,
+        'atk': 2400,
+        'def': 1500
+    },
+
+    'trap-hole': {
+        'id': 'trap-hole',
         'type': 'traps',
-        'file': 'dragon capture jar.png',
+        'subType': 'normal',
+        'file': 'trap_hole.png',
+        'name': 'Trap Hole',
+        'desc': 'When your opponent Normal or Flip Summons a monster with 1000 or more ATK, destroy that monster.'
+    },
+
+    'dragon-capture-jar': {
+        'id': 'dragon-capture-jar',
+        'type': 'traps',
+        'subType': 'continuous',
+        'file': 'dragon_capture_jar.png',
         'name': 'Dragon Capture Jar',
-        'desc': 'Change all face-up Dragon-Type monsters on the field to Defense Position. Draon-Type monsters cannot change back to Attack Position.'
+        'desc': 'Change all face-up Dragon-Type monsters on the field to Defense Position. Dragon-Type monsters cannot change back to Attack Position.'
     },
 
     'yami': {
+        'id': 'yami',
         'type': 'spells',
+        'subType': 'field',
         'file': 'yami.png',
         'name': 'Yami',
         'desc': 'Increase the ATK and DEF of all Fiend and Spellcaster-Type monsters by 300 points and decrease the ATK and DEF of all Fairy-Type monsters by 200 points.'
     },
 
     'wasteland': {
+        'id': 'wasteland',
         'type': 'spells',
+        'subType': 'field',
         'file': 'wasteland.png',
         'name': 'Wasteland',
         'desc': 'All Dinosaur, Zombie, and Rock monsters on the field gain 200 ATK/DEF.'
     },
 
-    'swords-of-relvealing-light': {
+    'mountain': {
+        'id': 'mountain',
         'type': 'spells',
-        'file': 'swords of revealing light.png',
+        'subType': 'field',
+        'file': 'mountain.png',
+        'name': 'Mountain',
+        'desc': 'Increase the ATK and DEF of all Dragon, Winged Beast, and Thunder-Type monsters by 200 points.'
+    },
+
+    'sogen': {
+        'id': 'sogen',
+        'type': 'spells',
+        'subType': 'field',
+        'file': 'sogen.png',
+        'name': 'Sogen',
+        'desc': 'Increase the ATK and DEF of all Warrior and Beast-Warrior-Type monsters by 200 points.'
+    },
+
+    'swords-of-revealing-light': {
+        'id': 'swords-of-revealing-light',
+        'type': 'spells',
+        'subType': 'normal',
+        'file': 'swords_of_revealing_light.png',
         'name': 'Swords of Revealing Light',
         'desc': "Flip all your opponent's face-down monsters to face-up. Destroy this card in 3 turns. While this card is face-up on the field, your opponent's monsters cannot declare an attack." 
     },
 
     'remove-trap': {
+        'id': 'remove-trap',
         'type': 'spells',
-        'file': 'remove trap.png',
+        'subType': 'normal',
+        'file': 'remove_trap.png',
         'name': 'Remove Trap',
         'desc': 'Destroy 1 face-up Trap Card on the field.'
     },
     
     'pot-of-greed': {
+        'id': 'pot-of-greed',
         'type': 'spells',
-        'file': 'pot of greed.png',
+        'subType': 'normal',
+        'file': 'pot_of_greed.png',
         'name': 'Pot of Greed',
         'desc': 'Draw 2 cards.'
     },
 
     'monster-reborn': {
+        'id': 'monster-reborn',
         'type': 'spells',
-        'file': 'monster reborn.png',
+        'subType': 'normal',
+        'file': 'monster_reborn.png',
         'name': 'Monster Reborn',
         'desc': 'Target 1 Monster in either Graveyard and Special Summon it.'
+    },
+
+    'dark-hole': {
+        'id': 'dark-hole',
+        'type': 'spells',
+        'subType': 'normal',
+        'file': 'dark_hole.png',
+        'name': 'Dark Hole',
+        'desc': 'Destroy all monsters on the field.'
+    },
+
+    'raigeki': {
+        'id': 'raigeki',
+        'type': 'spells',
+        'subType': 'normal',
+        'file': 'raigeki.png',
+        'name': 'Raigeki',
+        'desc': 'Destroy all monsters your opponent controls.'
+    },
+
+    'change-of-heart': {
+        'id': 'change-of-heart',
+        'type': 'spells',
+        'subType': 'normal',
+        'file': 'change_of_heart.png',
+        'name': 'Change of Heart',
+        'desc': 'Target 1 monster your opponent controls; take control of it until the End Phase.'
     }
+};
+
+// Helper schema functions
+function getCardDef(cardId) {
+    return cards[cardId] || null;
+}
+
+function isMonsterCard(cardId) {
+    var c = getCardDef(cardId);
+    return c && c.type === 'monsters';
+}
+
+function isSpellCard(cardId) {
+    var c = getCardDef(cardId);
+    return c && c.type === 'spells';
+}
+
+function isTrapCard(cardId) {
+    var c = getCardDef(cardId);
+    return c && c.type === 'traps';
 }
